ca4680843fa740409b82fd1eae47aa5669191866
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-07-11 13:25:21 +0100

    mongoDB debugging

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const PORT = 5001
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
+// // Johnexky2332, phAiCEgbAzsFhjfy
 let db, quotesCollection ;
 
 MongoClient.connect("mongodb+srv://ShabsCreativeContent01:phAiCEgbAzsFhjfy@cluster0.ehulrnp.mongodb.net/", { useUnifiedTopology: true })
@@ -44,101 +45,6 @@ app.post('/quotes', (request, response) => {
      })
      .catch(error => console.error(error))
 })
-
-
-// // Johnexky2332, phAiCEgbAzsFhjfy
-// let db,
-//     dbConnectionStr = "mongodb+srv://ShabsCreativeContent01:phAiCEgbAzsFhjfy@cluster0.ehulrnp.mongodb.net/",
-//     dbName = 'star-wars-quotes'
-//     dbCollection = db.collection('quotes')
-
-// // MongoClient.connect(dbConnectionSTr)
-// // .then(client => { 
-// //    console.log(`Connected to ${dbName} Database`)
-// //    db = client.db(dbName)
-// //    // const quotesCollection = db.collection('quotes')
-// // })
-// // .catch(error => console.error(error))
-
-// // // let db
-// // // let dbConnectionStr = "mongodb+srv://ShabsCreativeContent01:phAiCEgbAzsFhjfy@cluster0.ehulrnp.mongodb.net/"
-// // // let dbName = 'star-wars-quotes'
-
-// // const client = new MongoClient(dbConnectionStr)
-
-// // async function connect(){
-// //     try {
-// //         await client.connect();
-// //         db = client.db(dbName)
-// //         console.log("Connected to MongoDB");
-
-// //         app.listen(process.env.PORT || PORT, () => {
-// //             console.log(`Server running on port ${PORT}`)
-// //         })
-// //     }
-// //     catch (error) {
-// //         console.error(error);
-// //     }
-// // }
-
-// // connect();
-
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
-// // const client = new MongoClient(dbConnectionStr, {
-// //    serverApi: {
-// //        version: ServerApiVersion.v1,
-// //        strict: true,
-// //        deprecationErrors: true,
-// //    }
-// // })
-
-// // async function run() {
-// //    try {
-// //      // Connect the client to the server (optional starting in v4.7)
-// //      await client.connect();
-// //      // Send a ping to confirm a successful connection
-// //      db = await client.db(dbName).command({ ping: 1 });
-// //      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-// //    //   app.listen(process.env.PORT || PORT, function(){
-// //    //    console.log(`Server is running on port ${PORT}`);
-// //    // })
-// //    } finally {
-// //      // Ensures that the client will close when you finish/error
-// //      await client.close();
-// //    }
-// //  }
-
-// //  run().catch(console.dir);
-
-
-// const connectDB = async 
-
-// // app.get('/', (req, res) => {
-// //    res.send('<h1>Hello Word</h1>')
-// // })
-
-// app.get('/', (req, res) => {
-//    // res.sendFile(__dirname + '/index.html')
-
-//    dbCollection.find().toArray()
-//    .then(data => {
-//       // console.log(data)
-//     res.render('index.ejs', { quotes: data })
-//    })
-//    .catch(error => console.error(error))
-// })
-
-// // app.post('/newQuotes', (req,res) => {
-// //    // console.log(req.body);
-// //    db.collection('quotes').insertOne(req.body)
-// //    .then(result => {
-// //       console.log('Quotes Added')
-// //       res.redirect('/')
-// //    }).catch(error => console.error(error))
-// // })
 
    
 app.listen(process.env.PORT || PORT, function(){
