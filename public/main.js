cba42bf58a60ed3ec3c6ee6fc5cd379132e3b5f3
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-07-24 21:03:33 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -29,7 +29,7 @@ async function deleteQuotes(){
 async function updateLikes(){
    const fullName = this.parentNode.childNodes[1].innerText
    const fullQuote = this.parentNode.childNodes[3].innerText
-   const fullLikes = this.parentNode.childNodes[5].innerText
+   const fullLikes = Number(this.parentNode.childNodes[5].innerText)
 
    try{
       const response = await fetch('updateLikes', {
@@ -42,9 +42,9 @@ async function updateLikes(){
          })
       })
 
-      const data = response.json()
+      const data = await response.json()
       console.log(data)
-      location.reload()
+      // location.reload()
    }catch(error){
       console.error(error);
    }
